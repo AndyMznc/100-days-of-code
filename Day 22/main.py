45d0a254ed4bc5c5f2e821a26623cb3d5542564e
Andy Mezence <115222358+AndyMznc@users.noreply.github.com>
2025-03-13 12:26:18 +0100

    Finished: Pong game

--- Day 22/main.py
+++ Day 22/main.py
@@ -3,6 +3,7 @@ from turtle import Screen
 
 from ball import Ball
 from paddle import Paddle
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("black")
@@ -13,8 +14,9 @@ screen.tracer(0)
 
 left_paddle = Paddle((-350, 0))
 right_paddle = Paddle((350, 0))
-
 ball = Ball()
+scoreboard = Scoreboard()
+
 
 screen.listen()
 screen.onkey(left_paddle.go_up, "z")
@@ -25,8 +27,28 @@ screen.onkey(right_paddle.go_down, "Down")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if (
+        ball.distance(right_paddle) < 50
+        and ball.xcor() > 320
+        or ball.distance(left_paddle) < 50
+        and ball.xcor() < -320
+    ):
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
 
 screen.exitonclick()
